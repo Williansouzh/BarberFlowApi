a464b7b11ecfeb0d47e963dbc586499af99479b4
WillianSouzh <williansouza4041@gmail.com>
2025-03-11 21:39:28 -0300

    fix: dbContext

--- BarberFlowApi/Program.cs
+++ BarberFlowApi/Program.cs
@@ -1,18 +1,25 @@
 using Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
-
+using CrossCutting.IoC;
+//using CrossCutting.IoC;
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+builder.Services.AddInfrastructureAPI(builder.Configuration); 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+//builder.Services.AddInfrastructureAPI(builder.Configuration);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-string? mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+// string? mySqlConnection = builder.Configuration
+//                                 .GetConnectionString("DefaultConnection");
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-                    options.UseMySql(mySqlConnection,
-                    ServerVersion.AutoDetect(mySqlConnection)));
+// builder.Services.AddDbContext<AppDbContext>(options =>
+//                     options.UseMySql(mySqlConnection,
+//                     ServerVersion.AutoDetect(mySqlConnection)));
+
 
 var app = builder.Build();
 
